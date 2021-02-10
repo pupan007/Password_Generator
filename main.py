bb5a97d95f60e1c4c69ce20b50b7393aa552c417
pupan007 <jakub.rosner1@gmail.com>
2021-02-10 13:56:58 +0100

    added status bar

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from PIL import ImageTk, Image
 #Tkinter init
 root = Tk()
 root.title('Image Viewer')
-root.geometry('800x600')
 
 #images loader
 i=0
@@ -16,9 +15,10 @@ for image in os.listdir(directory):
         img = ImageTk.PhotoImage(Image.open(directory+image))
         images.append(img)
         
-#1st photo init
+#1st photo and status init
 photo = Label(image = images[i])
 photo.grid(row = 0, column= 0, columnspan = 3)
+status = Label(root, text = "Image " + str(i+1) + " of "+ str(len(images)), bd = 1, relief = SUNKEN, anchor = E)
 
 #utils
 def forward(len):
@@ -26,11 +26,14 @@ def forward(len):
     global i
     global button_forward
     global button_backward
+    global status
 
     i += 1
     photo.grid_forget()
     photo = Label(image = images[i])
     photo.grid(row = 0, column= 0, columnspan = 3)
+    status = Label(root, text = "Image " + str(i+1) + " of "+ str(len+1),relief = SUNKEN, anchor = E)
+    status.grid(row = 2, column =0, columnspan = 3, sticky = W+E)
     if i == len:
         button_forward['state'] = DISABLED
     if i != 0:
@@ -41,10 +44,13 @@ def backward(len):
     global photo
     global i
     global button_backward
+    global status
     i -= 1
     photo.grid_forget()
     photo = Label(image = images[i])
     photo.grid(row = 0, column= 0, columnspan = 3)
+    status = Label(root, text = "Image " + str(i+1) + " of "+ str(len+1),relief = SUNKEN, anchor = E)
+    status.grid(row = 2, column =0, columnspan = 3, sticky = W+E)
     if i == 0:
         button_backward['state'] = DISABLED
     
@@ -60,6 +66,7 @@ button_forward = Button(root, text=">>",padx = 50, command = lambda: forward(len
 #photo and button rendering
 button_backward.grid(row = 1, column= 0)
 button_exit.grid(row =1, column= 1)
-button_forward.grid(row = 1, column= 2)
+button_forward.grid(row = 1, column= 2, pady = 5)
+status.grid(row = 2, column =0, columnspan = 3, sticky = W+E)
 
 root.mainloop()
